9dd9acc1e2fc70de7f52b9570c557a0291fc007c
Abel <abel.carril@upm.es>
2024-11-30 19:48:02 +0100

    3.1.2

---
+++ FFBatch/Form6.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FFBatch
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_l = new System.Windows.Forms.Label();
+            this.pic_linux = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_linux)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pic1.Image = ((System.Drawing.Image)(resources.GetObject("pic1.Image")));
+            this.pic1.InitialImage = null;
+            this.pic1.Location = new System.Drawing.Point(8, 8);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(311, 75);
+            this.pic1.TabIndex = 12;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(130, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 18);
+            this.label1.TabIndex = 13;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_l
+            // 
+            this.lbl_l.Location = new System.Drawing.Point(13, 87);
+            this.lbl_l.Name = "lbl_l";
+            this.lbl_l.Size = new System.Drawing.Size(304, 17);
+            this.lbl_l.TabIndex = 14;
+            this.lbl_l.Text = "Loading wine/mono libraries, please wait";
+            this.lbl_l.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pic_linux
+            // 
+            this.pic_linux.Image = ((System.Drawing.Image)(resources.GetObject("pic_linux.Image")));
+            this.pic_linux.Location = new System.Drawing.Point(219, 26);
+            this.pic_linux.Name = "pic_linux";
+            this.pic_linux.Size = new System.Drawing.Size(16, 28);
+            this.pic_linux.TabIndex = 15;
+            this.pic_linux.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 50;
+            // 
+            // Form6
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(329, 114);
+            this.ControlBox = false;
+            this.Controls.Add(this.pic_linux);
+            this.Controls.Add(this.lbl_l);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form6";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form6_Load);
+            this.Shown += new System.EventHandler(this.Form6_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form6_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_linux)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pic_linux;
+        private System.Windows.Forms.Timer timer2;
+        public System.Windows.Forms.Label lbl_l;
+    }
+}
---
+++ FFBatch/Form6.cs
@@ -0,0 +1,145 @@
+﻿using FFBatch.Properties;
+using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+//using System.Threading;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FFBatch
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+
+        }
+        
+        private String port_path = System.IO.Path.Combine(Application.StartupPath, "settings") + "\\";
+        private Boolean is_portable = false;
+        private void Form6_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle borderRectangle = this.ClientRectangle;
+            //borderRectangle.Inflate(-2, -2);
+            ControlPaint.DrawBorder3D(e.Graphics, borderRectangle, Border3DStyle.Raised);                        
+        }
+
+        public void UpdateColorDark(Control myControl)
+        {
+            myControl.BackColor = Color.FromArgb(255, 64, 64, 64);
+            myControl.ForeColor = Color.White;
+            foreach (Control subC in myControl.Controls)
+            {
+                UpdateColorDark(subC);
+            }
+        }
+
+        private static bool IsNightLightEnabled()
+        {
+            const string BlueLightReductionStateKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\CloudStore\Store\DefaultAccount\Current\default$windows.data.bluelightreduction.bluelightreductionstate\windows.data.bluelightreduction.bluelightreductionstate";
+            using (var key = Registry.CurrentUser.OpenSubKey(BlueLightReductionStateKey))
+            {
+                var data = key?.GetValue("Data");
+                if (data is null)
+                    return false;
+                var byteData = (byte[])data;
+                return byteData.Length > 24 && byteData[23] == 0x10 && byteData[24] == 0x00;
+            }
+        }
+        private void Form6_Load(object sender, System.EventArgs e)
+        { 
+            if (Settings.Default.dark_os == false)
+            {
+                if (Settings.Default.dark_mode == true)
+                {
+                    foreach (Control c in this.Controls) UpdateColorDark(c);                    
+                }
+                if (Settings.Default.dark_mode == false && Settings.Default.auto_dark == true)
+                {
+                    String t_sunset0 = Settings.Default.dark_sunset.ToShortTimeString();
+                    String t_sunset1 = t_sunset0.Substring(0, t_sunset0.IndexOf(":"));
+                    String t_sunset2 = t_sunset0.Substring(t_sunset0.IndexOf(":") + 1, t_sunset0.Length - t_sunset0.IndexOf(":") - 1);
+                    TimeSpan sunset_t = new TimeSpan(Convert.ToInt32(t_sunset1), Convert.ToInt32(t_sunset2), 0);
+                    DateTime sunset0 = DateTime.Now.Date + sunset_t;
+
+                    String t_sunrise0 = Settings.Default.dark_sunrise.ToShortTimeString();
+                    String t_sunrise1 = t_sunrise0.Substring(0, t_sunrise0.IndexOf(":"));
+                    String t_sunrise2 = t_sunrise0.Substring(t_sunrise0.IndexOf(":") + 1, t_sunrise0.Length - t_sunrise0.IndexOf(":") - 1);
+                    TimeSpan sunrise_t = new TimeSpan(Convert.ToInt32(t_sunrise1), Convert.ToInt32(t_sunrise2), 0);
+                    DateTime sunrise0 = DateTime.Now.Date + sunrise_t;
+
+                    TimeSpan r_sunset = DateTime.Now - sunset0;
+                    TimeSpan r_sunrise = DateTime.Now - sunrise0;
+
+                    if (r_sunset.TotalSeconds > 0 && r_sunrise.TotalSeconds < 0)
+                    {
+                        foreach (Control c in this.Controls) UpdateColorDark(c);                        
+                    }
+                }
+            }
+
+            if (Settings.Default.dark_os == true && Settings.Default.dark_mode == false)
+            {
+                if (IsNightLightEnabled() == true)
+                {
+                    Settings.Default.dark_mode = true;
+                }
+                else Settings.Default.dark_mode = false;
+            }
+
+            if (Properties.Settings.Default.dark_mode == true)
+            {
+                foreach (Control c in this.Controls) UpdateColorDark(c);
+                this.BackColor = Color.FromArgb(255, 64, 64, 64);                
+            }
+
+            String app_location = Application.StartupPath;
+            String portable_flag = Application.StartupPath + "\\" + "portable.ini";
+            if (File.Exists(portable_flag)) is_portable = true;
+            else is_portable = false;
+
+            //Startup delay
+
+            String f_delay = String.Empty;
+            if (is_portable == false)
+            {
+                f_delay = System.IO.Path.Combine(Environment.GetEnvironmentVariable("appdata"), "FFBatch") + "\\" + "ff_delay.ini";
+            }
+            else
+            {
+                f_delay = port_path + "ff_delay_portable.ini";
+            }
+
+            if (File.Exists(f_delay))
+            {
+                int i = Convert.ToInt32(File.ReadAllText(f_delay));
+                label1.Visible = true;
+                label1.Text = Properties.Strings.delay_act + ": " + i.ToString();
+
+            }
+            //End startup delay
+            timer1.Start();
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form6_Shown(object sender, EventArgs e)
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
